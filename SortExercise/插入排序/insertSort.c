34a39c031aa9070d372b36240d556768d1e7f8b5
常昊 <changhao@xiyou3g.com>
2018-03-20 22:46:53 +0800

    2018-3-20

--- SortExercise/插入排序/insertSort.c
+++ SortExercise/插入排序/insertSort.c
@@ -8,5 +8,15 @@
 #include "insertSort.h"
 void insertSort(int array[],int lenth,int isMoreBigger)
 {
-    int 
+    int i,j,temp;
+    for(i=lenth;i>0;i--)
+    {
+        for(j=i-1;j>0;j--)
+            if(array[i]<array[j])
+            {
+                temp=array[i];
+                array[i]=array[j];
+                array[j]=temp;
+            }
+    }
 }
