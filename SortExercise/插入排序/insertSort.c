c522d8cdb256224e3ac75e5664edad33a37129a7
常昊 <changhao@xiyou3g.com>
2018-03-21 10:43:25 +0800

    插入排序

--- SortExercise/插入排序/insertSort.c
+++ SortExercise/插入排序/insertSort.c
@@ -6,17 +6,65 @@
 //
 
 #include "insertSort.h"
-void insertSort(int array[],int lenth,int isMoreBigger)
+
+void sortInsert(int array[],int lenth,int isMoreBigger)
 {
-    int i,j,temp;
-    for(i=lenth;i>0;i--)
+    int i;
+    int j;
+    int temp;
+    
+    if(!array||!lenth)
+        return ;
+    
+    if(isMoreBigger==0)
     {
-        for(j=i-1;j>0;j--)
-            if(array[i]<array[j])
+        for(i=0;i<lenth;i++)
+            for(j=i;j>0;j--)
             {
-                temp=array[i];
-                array[i]=array[j];
-                array[j]=temp;
+                if(array[j]<array[j-1])
+                {
+                    temp=array[j];
+                    array[j]=array[j-1];
+                    array[j-1]=temp;
+                }
+            }
+    }
+    else
+    {
+        for(i=0;i<lenth;i++)
+            for(j=i;j>0;j--)
+            {
+                if(array[j]>array[j-1])
+                {
+                    temp=array[j];
+                    array[j]=array[j-1];
+                    array[j-1]=temp;
+                }
             }
     }
 }
+
+int main()
+{
+    int array[100];
+    char s;
+    int i;
+    int lenth=0;
+    for(i=0;i<100&&s!='\n';i++)
+    {
+        scanf("%d",array+i);
+        s=getchar();
+        lenth++;
+    }
+    
+    sortInsert(array,lenth,1);
+    
+    for(i=0;i<lenth;i++)
+    {
+        printf("%d ",array[i]);
+    }
+    
+    
+    return 0;
+}
+
